935155d288bd848588f23c22627d22e27668b7fd
Niklesh Shakya <uniqsaqya@gmail.com>
2019-05-12 12:36:42 +0545

    Added search for recipe

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
-
+import RecipeList from "../components/RecipeList";
+import Search from "../components/Search";
+import { recipeData } from "../data/tempDetails";
 export default class Recipes extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    recipes: recipeData,
+    search: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
   render() {
-    return <div>Hello from Receipes</div>;
+    return (
+      <>
+        <Search
+          search={this.state.search}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <RecipeList recipes={this.state.recipes} />
+      </>
+    );
   }
 }
