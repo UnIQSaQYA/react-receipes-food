81be4450307d67e104063a0a1c29176e2964fa3b
Niklesh Shakya <uniqsaqya@gmail.com>
2019-05-12 09:10:00 +0545

    Project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import RecipeDetail from "./pages/Detail";
+import Default from "./pages/Default";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 export default class App extends Component {
   render() {
-    return <div>Hell from receipe</div>;
+    return (
+      <Router>
+        <main>
+          {/* navbar */}
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="reciepes/:id" component={RecipeDetail} />
+            <Route component={Default} />
+          </Switch>
+        </main>
+      </Router>
+    );
   }
 }
